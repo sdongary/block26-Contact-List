7943419f8ef340db2ff28a0aa9412d01d5296103
Sachin Dongary <siddharth.sachin89@gmail.com>
2024-02-01 20:05:47 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import ContactList from './components/ContactList'
+
+
+function App() {
+  
+  return (
+    <>
+      <ContactList />
+      
+    </>
+  )
+}
+
+export default App
